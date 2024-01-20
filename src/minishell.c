193e3621baa8361599582286490653a6e740bb6c
Maria Avoletta <mloa182610@gmail.com>
2024-01-20 19:54:51 +0000

    note: new branch created, adding some random useful file

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: mariaavoletta <mariaavoletta@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 20:51:51 by arabelo-          #+#    #+#             */
-/*   Updated: 2024/01/16 20:02:29 by mariaavolet      ###   ########.fr       */
+/*   Updated: 2024/01/20 19:44:47 by mariaavolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	init_shell(t_terminal terminal)
 	{
 		// if (i++ == 2)
 		// 	return (0);
-		terminal.prompt = readline("minishell>");
+		terminal.prompt = readline("minishell> ");
 		if (!ft_strlen(terminal.prompt))
 		{
 			reset_terminal(&terminal, true);
@@ -29,7 +29,7 @@ void	init_shell(t_terminal terminal)
 		if (!lexer(&terminal))
 			continue ;
 		parser(&terminal);
-		visualize_commands(terminal.commands);
+		//visualize_commands(terminal.commands);
 		free_structs(&terminal, false, NULL);
 		reset_terminal(&terminal, SUCCESS);
 	}
@@ -48,12 +48,12 @@ int	main(int ac, char **av, char **env_path)
 	i = -1;
 	while(env_path && env_path[++i])
 		env = env_structure(env_path[i], env);
-	while(env && env->next)
-	{
-		printf("\n[\n%s\n", env->info);
-		printf("%p]", env);
-		env = env->next;
-	}
+	// while(env && env->next)
+	// {
+	// 	printf("\n[\n%s\n", env->info);
+	// 	printf("%p]", env);
+	// 	env = env->next;
+	// }
 	init_terminal(&terminal, true);
 	terminal.env = env;
 	init_shell(terminal);

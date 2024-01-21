705ac57e63d515099818c7634a329938813a67ba
Maria Avoletta <mloa182610@gmail.com>
2024-01-21 18:58:13 +0000

    fix: ft_expansion_check

--- src/expander/expander.c
+++ src/expander/expander.c
@@ -6,13 +6,11 @@
 /*   By: mariaavoletta <mariaavoletta@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 21:40:19 by mariaavolet       #+#    #+#             */
-/*   Updated: 2024/01/21 14:43:47 by mariaavolet      ###   ########.fr       */
+/*   Updated: 2024/01/21 18:50:28 by mariaavolet      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/* dar handle do que é permitido, ao inves do que nao pode */
 
 bool	ft_forbidden_expansion(char c, int i)
 {
@@ -25,11 +23,11 @@ bool	ft_forbidden_expansion(char c, int i)
 
 char	*variable_alias(char *str)
 {
-	char	*expand_var;
+	char	*expand_var; 
 	int		i;
 
 	i = 0;
-	while(str[++i])
+	while(str && str[++i])
 	{
 		if((str[i] == '?' && i == 1) || ft_forbidden_expansion(str[i], i) == true)
 			break ;
@@ -50,14 +48,12 @@ char	*ft_search_variable(char *var, t_terminal *terminal)
 	env = terminal->env;
 	while (env->next)
 	{
-		printf("env: %s\n", env->info);
 		if (!ft_strncmp(var, env->info, ft_strlen(var)))
 			break ;
 		env = env->next;
 	}
 	if (!env)
 		return(ft_strdup(""));
-	printf("dup: %s\n", ft_strdup(env->info + (ft_strlen(var) + 1)));
 	return (ft_strdup(env->info + (ft_strlen(var) + 1)));
 }
 
@@ -68,7 +64,7 @@ char	*ft_should_expand(char *str, int *i, t_terminal *terminal)
 	expand_var = variable_alias(&str[*i]);
 	if (ft_strncmp(expand_var, "$", ft_strlen("$")))
 	{
-		*i += ft_strlen(expand_var) + 1; /* verificar se precisa mesmo do +1 */
+		*i += ft_strlen(expand_var) + 1;
 		expand_var = ft_search_variable(expand_var, terminal);
 		if (!expand_var)
 		{
@@ -83,7 +79,6 @@ char	*ft_should_expand(char *str, int *i, t_terminal *terminal)
 		*i += 1;
 	if (!ft_strncmp(expand_var, "?", ft_strlen("?")))
 		expand_var = ft_itoa(terminal->exit_status);
-	//printf(">>\n%s\n<<\n", expand_var);
 	return (expand_var);
 	
 }
@@ -94,10 +89,19 @@ char	ft_checking_quotes(char c, char flag)
 	{
 		if (!flag)
 			flag = c;
-		else	
+		else
 			flag = '\0';
 	}
 	return (flag);
+}
+
+char	*ft_join_free(char *test, int i, char *temp, t_terminal *terminal)
+{
+	char	*var_key;
+	free(temp);
+	temp = test;
+	var_key = ft_strjoin(temp, ft_substr(terminal->tokens->token, i, ft_strlen(terminal->tokens->token)));
+	return(var_key);
 }
 
 char	*ft_expansion_check(t_terminal *terminal, char flag)
@@ -105,8 +109,11 @@ char	*ft_expansion_check(t_terminal *terminal, char flag)
 	int			i;
 	char		*var_key;
 	char		*temp;
-	
-	var_key = ft_strdup(terminal->tokens->token);
+	char		*new_index;
+	char		*test;
+
+	new_index = 0;
+	var_key = ft_strdup(terminal->tokens->token); //**$USER
 	i = 0;
 	while (terminal->tokens && terminal->tokens->token[i])
 	{
@@ -116,26 +123,10 @@ char	*ft_expansion_check(t_terminal *terminal, char flag)
 			i++;
 			continue ;
 		}
-		temp = ft_substr(var_key, 0, i);
-		free(var_key);
-		printf("*********************************************\n");
-		printf("index = %i\n", i);
-		temp = ft_strjoin(temp, ft_should_expand(terminal->tokens->token, &i, terminal));
-		printf("index = %i\n", i);
-		var_key = ft_strjoin(temp, ft_substr(terminal->tokens->token, i, ft_strlen(terminal->tokens->token)));
-		
+		temp = ft_substr(var_key, 0, ft_strlen(new_index));
+		new_index = ft_should_expand(terminal->tokens->token, &i, terminal);
+		test = ft_strjoin(temp, new_index);
+		var_key = ft_join_free(test, i, temp, terminal);
 	}
-	//free(temp);
 	return (var_key);
 }
-
-
-
-/* if (terminal.tokens->token[i] == SINGLE_QUOTE || \
-			terminal.tokens->token[i] == DOUBLE_QUOTE)
-		{
-			if (!flag)
-				flag = terminal.tokens->token[i];
-			else	
-				flag = '\0';
-		} */
